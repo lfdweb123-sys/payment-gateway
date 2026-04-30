9ec1ab5c822767283b091828595af614a23d8b5b
Gerard Sononkpon <lfdweb123@gmail.com>
2026-04-30 17:46:36 +0100

    Update pollStatus to use transactionId instead of reference

--- src/pages/gateway/GatewayPay.jsx
+++ src/pages/gateway/GatewayPay.jsx
@@ -533,7 +533,7 @@ const handleSubmit = async (e) => {
       setLoading(false);
       if (isMobile && fullPhone) savePhoneNumber(fullPhone);
       if (data.url) window.location.href = data.url;
-      else pollStatus(data.reference);
+      else pollStatus(data.transactionId);
     } else {
       toast.error(data.error || 'Une erreur est survenue');
       setLoading(false);
